f291d38d1657006b2de963fb1f7360bd0c0e3311
Esther Carrelle <esthcarelle@gmail.com>
2024-01-25 23:02:19 +0200

    Override on resume method and initialize exo player when SDK <= 23

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -55,4 +55,12 @@ class PlayerActivity : AppCompatActivity() {
             initializePlayer()
         }
     }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer()
+        }
+    }
 }
