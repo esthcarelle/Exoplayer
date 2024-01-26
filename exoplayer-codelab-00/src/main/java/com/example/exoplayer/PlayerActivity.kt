457f0ae2d0fa86d5d50f9e2dc3ec34416906a80e
Esther Carrelle <esthcarelle@gmail.com>
2024-01-26 15:44:41 +0200

    initialize the media item index to 0

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -38,6 +38,7 @@ class PlayerActivity : AppCompatActivity() {
 
     private var player: ExoPlayer? = null
     private var playWhenReady = true
+    private var mediaItemIndex = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
